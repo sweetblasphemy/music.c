affb82680ee37619a6aad997411ff7d323516fc5
sweetblasphemy <nik2006@abv.bg>
2023-05-13 11:38:42 +0300

    Add project files.

---
+++ music.c/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace music.c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            music m = new music("Pantera", 57.38, "Cowboys from Hell");
+            music m1 = new music("Korn", 53.20, "Issues");
+            music m2 = new music("Slayer", 41.56, "Repentless");
+
+            Console.WriteLine("The length of the albums is: ");
+            Console.WriteLine(Math.Round(m.length + m1.length + m2.length, 2));
+        }
+    }
+}
---
+++ music.c/music.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music.c
+{
+    internal class music
+    {
+
+        public string author;
+        public double length;
+        public string album_name;
+
+        public music(string author, double length, string album_name)
+        {
+
+            this.author = author;
+            this.length = length;
+            this.album_name = album_name;
+            Console.WriteLine("The author is: " + author + ", the album's length is: " + length + ", the album's name is: " + album_name);
+            
+        }
+    }
+}
